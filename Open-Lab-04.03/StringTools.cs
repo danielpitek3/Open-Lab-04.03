6b57d0349dc0d7f24a303fc90053a667b89d6cc3
Daniel Pitek <Daniel813245@outlook.sk>
2021-11-15 18:12:20 +0100

    done

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-
+using System.Runtime;
+using System.Linq;
 namespace Open_Lab_04._03
 {
     public class StringTools
     {
-        public string RemoveFirstLast(string original)
+        public string RemoveFirstLast(string original_String)
         {
-            throw new NotImplementedException();
+            if (original_String.Length > 2)
+            {
+                string result = original_String.Substring(1, original_String.Length - 2);
+                return result;
+            }
+            else if (original_String.Length == 1)
+            {
+                return original_String; 
+            }
+            else
+            {
+                return string.Empty; 
+            }
+
+
         }
     }
 }
